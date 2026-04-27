09635c4c54fdb6a4fb3e972412ee068a96ec34d0
Bee <kea240000@utdallas.edu>
2026-04-27 13:29:20 -0500

    Created the turntable option button code, and will begin to implement further into main layout.

--- Turntable_P2.py
+++ Turntable_P2.py
@@ -17,6 +17,7 @@ class TurnTableWin(QtWidgets.QDialog):
     def __init__(self):
     #put in intialization code for the window creation.
         super().__init__(parent=get_maya_main_win())
+        self.turntable = TurnTable()
         self.setWindowTitle("Turntable Generator")
         self.resize(300, 200)
         self._mk_main_layout()
@@ -28,6 +29,7 @@ class TurnTableWin(QtWidgets.QDialog):
         self.setLayout(self.main_layout)
     def generate_turntable(self):
     #Pass the values of what is input in the GUI to the rest of the code.
+
 
     def connect_signals(self):
     #Connect button click signals to do as requested, like close the window.
@@ -41,11 +43,32 @@ class TurnTableWin(QtWidgets.QDialog):
         self.main_layout.addWidget(self.build_btn)
         self.main_layout.addWidget(self.cancel_btn)
 
+    def turntable_options(self):
+        #Create the buttons for the user to place values.
+        self.fps_label = QtWidgets.QLabel("FPS:")
+        self.fps_input = QtWidgets.QSpinBox()
+        self.fps_input.setRange(12, 36)
+        self.seconds_label = QtWidgets.QLabel("Seconds:")
+        self.seconds_input = QtWidgets.QSpinBox()
+        self.seconds_input.setRange(1, 30)
+        self.rps_label = QtWidgets.QLabel("RPS:")
+        self.rps_input = QtWidgets.QDoubleSpinBox()
+        self.rps_input.setRange(0.1, 1.0)
+        self.preset_lights_checkbox = QtWidgets.QCheckBox("Preset Lights")
+        self.main_layout.addWidget(self.fps_label)
+        self.main_layout.addWidget(self.fps_input)
+        self.main_layout.addWidget(self.seconds_label)
+        self.main_layout.addWidget(self.seconds_input)
+        self.main_layout.addWidget(self.rps_label)
+        self.main_layout.addWidget(self.rps_input)
+        self.main_layout.addWidget(self.preset_lights_checkbox)
+
 class TurnTable():
     #Gather the FPS input and place keyframes at the desired points
     FPS = 24
     Seconds = 5
     RPS = .25
+    Preset_Lights = True
     def get_selection(self):
     #Select objects for the generator to turn.
         objects = cmds.ls(selection=True)
